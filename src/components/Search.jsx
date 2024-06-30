76098399b23ddc7dbb8abf772a590edc0d1782cb
nitinya9av <nitinya9av@gmail.com>
2024-06-30 17:20:15 +0530

    Api-md5-Hashing_Added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
+import md5 from "md5";
 import "../styles/Search.scss";
 
 function Search() {
+  const [characterName, setCharacterName] = useState("");
+  const [characterData, setCharacterData] = useState(null);
+
+  const privateKey = import.meta.env.VITE_MARVEL_PRIVATE_KEY;
+  const publicKey = import.meta.env.VITE_MARVEL_PUBLIC_KEY;
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     getCharacterData();
+  };
+
+  const getCharacterData = () => {
+    setCharacterData(null);
+    setComicData(null);
+
+    const timeStamp = new Date().getTime();
+    const hash = generateHash(timeStamp);
+
+    const url = `https://developer.marvel.com:443/v1/public/characters?apikey=${publicKey}&hash=${hash}&ts=${timeStamp}&nameStartsWith=${characterName}&limit=100`;
+
+  };
+
+  const generateHash = (timeStamp) => {
+    return md5(timeStamp + privateKey + publicKey);
   };
 
   const handleChange = (event) => {
@@ -17,7 +39,7 @@ function Search() {
     setCharacterData(null);
     setComicData(null);
   };
-  
+
   return (
     <>
       <form className="search" onSubmit={handleSubmit}>

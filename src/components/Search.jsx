4a9eb29af81ead017130c6a11e852883014d7d8d
nitinya9av <nitinya9av@gmail.com>
2024-06-30 16:23:53 +0530

    handelsAdded

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,34 @@
 import "../styles/Search.scss";
 
 function Search() {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    getCharacterData();
+  };
+
+  const handleChange = (event) => {
+    setCharacterName(event.target.value);
+  };
+
+  const handleReset = () => {
+    setCharacterName("");
+    setCharacterData(null);
+    setComicData(null);
+  };
+  
   return (
     <>
-      <form className="search">
+      <form className="search" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="enter character name"
+          onChange={handleChange}
         />
         <div className="buttons">
           <button type="submit">Get Character Data</button>
-          <button type="reset" className="reset">
+          <button type="reset" className="reset" onClick={handleReset}>
             Reset
           </button>
         </div>

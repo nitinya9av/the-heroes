9a3ca805ca09b48cbedd0193f6f7e8b8570a6ebf
nitinya9av <nitinya9av@gmail.com>
2024-07-01 06:24:07 +0530

    comicsIncomplete

--- src/components/Comics.jsx
+++ src/components/Comics.jsx
@@ -1,6 +1,23 @@
+
 function Comics() {
   return (
-    <div></div>
+    <div className="comics">
+      {data.map((dataItem) => {
+        const detailsUrl = dataItem.urls.find(
+          (element) => element["type"] === "detail"
+        ).url;
+
+        return (
+          <a
+            key={dataItem.id}
+            className="comicCard"
+          >
+            <div className="caption">{dataItem.title}</div>
+            <div className="caption bottom">View Comic Details</div>
+          </a>
+        );
+      })}
+    </div>
   );
 }
 

79f7822cad8d346fa0f01bac3faee94aa15fcdd1
nitinya9av <nitinya9av@gmail.com>
2024-07-01 05:37:15 +0530

    charactersData_fetch

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,6 +1,20 @@
-function Characters() {
+
+function Characters({ data, onClick }) {
   return (
-    <div></div>
+    <div className="characters">
+      {data.map((dataItem) => {
+        return (
+          <div
+            key={dataItem.id}
+            className="characterCard"
+            onClick={() => onClick(dataItem.id)}
+          >
+            <div className="caption">{dataItem.name}</div>
+            <div className="caption bottom">View Comics</div>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 

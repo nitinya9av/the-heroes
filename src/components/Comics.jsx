3aeae2b5c91b27853f2ba1d10cefc15ba04c8583
nitinya9av <nitinya9av@gmail.com>
2024-07-01 07:57:56 +0530

    comicsComplete

--- src/components/Comics.jsx
+++ src/components/Comics.jsx
@@ -1,5 +1,5 @@
 
-function Comics() {
+function Comics({ data, onClick }) {
   return (
     <div className="comics">
       {data.map((dataItem) => {
@@ -11,6 +11,14 @@ function Comics() {
           <a
             key={dataItem.id}
             className="comicCard"
+            style={{
+              background: `url(${dataItem.thumbnail.path}.${dataItem.thumbnail.extension}) no-repeat center`,
+              backgroundSize: "cover",
+            }}
+            onClick={() => onClick(dataItem.id)}
+            href={detailsUrl}
+            target="_blank"
+            rel="noreferrer"
           >
             <div className="caption">{dataItem.title}</div>
             <div className="caption bottom">View Comic Details</div>
